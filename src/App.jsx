d67e77446eca658f2ed65142a2422d8bbbf93df8
bpsrm <bankppt.srm@gmail.com>
2023-10-05 17:26:49 +0700

    customize scrollbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,48 @@
 import { Main } from "./pages/Main";
 
+import { useState, useEffect, Suspense } from "react";
+import { BarLoader } from "react-spinners";
+
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  const containerStyle = {
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  };
+
   return (
-    <div>
-      <Main />
-    </div>
+    <Suspense
+      fallback={
+        <div
+          className="w-100 d-flex justify-content-center align-items-center"
+          style={containerStyle}
+        >
+          <BarLoader className="w-25 loader" color="#854ce6" height={5} />
+        </div>
+      }
+    >
+      {loading ? (
+        <div
+          className="w-100 d-flex justify-content-center align-items-center"
+          style={containerStyle}
+        >
+          <BarLoader className="w-25 loader" color="#854ce6" height={5} />
+        </div>
+      ) : (
+        <Main />
+      )}
+    </Suspense>
   );
 };
 
